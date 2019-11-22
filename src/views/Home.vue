4f4e2c6ec8569d37c75b213158771a9691c4b55e
未央 <hiveforwork@163.com>
2019-11-22 23:32:24 +0800

    对topic列表页进行部分适配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <table class="table" border="1">
+        <table class="table">
             <tr class="tr_head border_bottom">
                 <th class="th_title" align="left" colspan="2">
                     <div class="title_box">全部主题</div>
@@ -8,18 +8,15 @@
                 <!--<th align="center">作者</th>
                 <th align="center">回复 / 查看</th>
                 <th align="center">最后发表</th>-->
-                <!--<th align="center"></th>
-                <th align="center"></th>
-                <th align="center"></th>-->
             </tr>
-            <tr class="tr_topic" v-for="(topic, index) in topicList" :key="index">
+            <!--<tr class="tr_topic" v-for="(topic, index) in topicList" :key="index">
                 <td :class="index==topicList.length-1?'label':'label border_bottom'" align="center">
                     【<span>{{ topic.label }}</span>】
                 </td>
                 <td :class="index==topicList.length-1?'title':'title border_bottom'" colspan="3">
                     <router-link :to="'/toTopic?id='+topic.id">{{ topic.title }}</router-link>
                 </td>
-                <!--<td :class="index==topicList.length-1?'author':'author border_bottom'">
+                <td :class="index==topicList.length-1?'author':'author border_bottom'">
                     <h3>{{ topic.username }}</h3>
                     <p>{{ topic.submittime | dateFormat }}</p>
                 </td>
@@ -30,8 +27,21 @@
                 <td :class="index==topicList.length-1?'latestReply':'latestReply border_bottom'">
                     <h3>{{ topic.lastReply }}</h3>
                     <p>{{ topic.lastSubmit | dateFormat }}</p>
-                </td>-->
-            </tr>
+                </td>
+            </tr>-->
+            <div class="table_topic" v-for="(topic, index) in topicList" :key="index">
+                <div>
+                    【<span class="label">{{ topic.label }}</span>】
+                    <span class="submitTime">{{ topic.submittime | dateFormat }}</span>
+                </div>
+                <div class="title">{{ topic.title }}</div>
+                <div class="author_count">
+                    <!--<span>{{ topic.username }}</span>
+                    <span>{{ topic.replycount }}</span>-->
+                    <div class="author">{{ topic.username }}</div>
+                    <div class="count">{{ topic.replycount }}</div>
+                </div>
+            </div>
         </table>
         <div class="paging_box">
             <Page v-if="paging.total>0" :current="paging.currentPage" :page-size="paging.pageSize"
@@ -116,33 +126,36 @@
         border-left: 6px solid dodgerblue;
     }
 
-    .tr_topic {
-        height: 60px;
+    .table_topic {
+        height: 90px;
+        margin-top: 5px;
     }
 
     .label {
         width: 14%;
-        font-size: 1.1em;
-        span {
-            color: #ff78f2;
-        }
+        font-size: 1.2em;
+        color: #ff78f2;
+    }
+
+    .submitTime {
+        font-size: 1.0em;
+        /*font-weight: bold;*/
     }
 
     .title {
-        width: 86%;
         font-size: 1.2em;
         font-weight: bold;
-
-        a {
-            color: #515a6e;
-        }
-
-        a:hover {
-            color: #2d8cf0;
-        }
     }
 
     .author {
+        float: left;
+    }
+
+    .count {
+        float: right;
+    }
+
+    /*.author {
         text-align: center;
 
         h3 {
@@ -152,7 +165,7 @@
         p {
             color: #ffa41e;
         }
-    }
+    }*/
 
     .reply_view {
         text-align: center;
